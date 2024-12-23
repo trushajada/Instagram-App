180456c5ffb42d157db677c319971b6c36df3eb1
Your Name <you@example.com>
2024-12-24 05:06:02 +0530

    header commit

---
+++ src/Componets/SingIn/SingIn.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const SingIn=()=>{
+    return(
+        <>
+        
+        </>
+    )
+}
+export default SingIn
---
+++ src/Componets/SingUp/SingUp.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const SingUp=()=>{
+    return(
+        <>
+        
+        </>
+    )
+}
+export default SingUp
